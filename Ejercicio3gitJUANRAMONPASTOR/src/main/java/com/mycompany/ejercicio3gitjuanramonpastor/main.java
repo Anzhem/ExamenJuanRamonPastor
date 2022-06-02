fd2b219a93dc025080298ef7733b7b65bf6f3a5d
Juan Ramón Pastor Sánchez <2021355@iesluisbraille.es>
2022-06-02 10:37:40 +0200

    primer commit

---
+++ Ejercicio3gitJUANRAMONPASTOR/src/main/java/com/mycompany/ejercicio3gitjuanramonpastor/main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ejercicio3gitjuanramonpastor;
+import java.util.*;
+/**
+ *
+ * @author JuanRamónPastorSánch
+ */
+public class main {
+    public static void mostrarPares(){
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduzca el número por teclado");
+        int total = teclado.nextInt();
+        int i;
+        for (i=1;i>total;i++){
+            if(i%2!=0){
+                System.out.println(i);
+            }
+        }
+    }
+}
